a0022cefba3f443787ace0aa108f412306c0a1f5
baliwoods <balihehar@gmail.com>
2020-07-06 21:14:07 -0700

    imlemented

--- game/SingleGame.go
+++ game/SingleGame.go
@@ -1,1 +1,10 @@
 package game
+
+func GetGame(gameID int) (Game, error) {
+	gameCopy, err := findGameByID(gameID)
+	if err == nil {
+		emptyArr := [4]int{}
+		gameCopy.Answer = emptyArr
+	}
+	return gameCopy, err
+}
--- game/SingleGame_test.go
+++ game/SingleGame_test.go
@@ -1,1 +1,29 @@
 package game
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetGame(t *testing.T) {
+	Begin()
+	got, err := GetGame(0)
+	expected := Game{0, [4]int{}, "inProgress", make([]Round, 1)}
+
+	if err != nil {
+		t.Errorf("Got %v erro but expected nil", err)
+	}
+
+	if got.ID != expected.ID {
+		t.Errorf("Got id %v Expected id %v", got.ID, expected.ID)
+	}
+	if got.Answer != expected.Answer {
+		t.Errorf("Got answer %v but Expected %v", got.Answer, expected.Answer)
+	}
+	if reflect.DeepEqual(got.Rounds, expected.Rounds) == false {
+		t.Errorf("Got rounds %v Expected %v", got.Rounds, expected.Rounds)
+	}
+	if got.Status != expected.Status {
+		t.Errorf("Got status %v Expected %v", got.Status, expected.Status)
+	}
+}
