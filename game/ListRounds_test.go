e33f39be9207de3e88f1d3b9a2ee342931890693
Jasmin Smith <jasmith.07.13.99@gmail.com>
2020-07-07 00:22:41 -0400

    We are on to somthing bois

--- game/ListRounds_test.go
+++ game/ListRounds_test.go
@@ -1,1 +1,30 @@
 package game
+
+import (
+	"testing"
+	"time"
+)
+
+func TestListRounds(t *testing.T) {
+	round1 := Round{
+		1,
+		1,
+		3,
+		1,
+		time.Now(),
+	}
+	var round []Round
+	round = append(round, round1)
+	game1 := Game{
+		0,
+		[4]int{1, 2, 3, 4},
+		"IN PROGRESS",
+		round,
+	}
+
+	ListRounds(0)
+	if game1.Rounds[0] != round1 {
+		t.Errorf("Expected round[0] to be %v but got %v", game1.Rounds[0], round1)
+	}
+
+}
