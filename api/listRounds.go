249574785600c43bc28acd8c54cb7434831d709d
Andrew Horn <arhorn@smcm.edu>
2020-07-07 12:43:04 -0500

    added GET rounds endpoint

--- api/listRounds.go
+++ api/listRounds.go
@@ -1,7 +1,30 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"../game"
+	"github.com/gorilla/mux"
+)
+
+type data struct {
+	Rounds []game.Round `json:"rounds"`
+}
 
 func listRounds(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	gameID := vars["gameId"]
+	id, err := strconv.Atoi(gameID)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	g, err := game.GetGame(id)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+	json.NewEncoder(w).Encode(g.Rounds)
 }
