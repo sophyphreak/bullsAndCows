ec0003f3aa02a4d60ab5b47be3df71e253987a82
baliwoods <balihehar@gmail.com>
2020-07-07 08:33:28 -0700

    implemented singleGame

--- api/singleGame.go
+++ api/singleGame.go
@@ -1,7 +1,17 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"../game"
+	"github.com/gorilla/mux"
+)
 
 func singleGame(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	gameId := vars["gameId"]
+	foundGame, err := game.GetGame(gameId)
+	check(err)
+	json.NewEncoder(w).Encode(foundGame)
 }
