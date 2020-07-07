d466e4f78209515685a11906f25510676b5dfef9
baliwoods <balihehar@gmail.com>
2020-07-06 21:21:44 -0700

    check for game status

--- game/SingleGame.go
+++ game/SingleGame.go
@@ -3,8 +3,10 @@ package game
 func GetGame(gameID int) (Game, error) {
 	gameCopy, err := findGameByID(gameID)
 	if err == nil {
-		emptyArr := [4]int{}
-		gameCopy.Answer = emptyArr
+		if gameCopy.Status = "inProgress" {
+			emptyArr := [4]int{}
+			gameCopy.Answer = emptyArr
+		}
 	}
 	return gameCopy, err
 }
