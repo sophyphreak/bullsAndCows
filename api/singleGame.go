73c888b741c87752fb6f2a0fb468b6c1cffc3421
Andrew Horn <arhorn@smcm.edu>
2020-07-07 10:45:54 -0500

    resolved merge conflict

--- api/singleGame.go
+++ api/singleGame.go
@@ -1,17 +1,8 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
-<<<<<<< HEAD
-)
-
-func singleGame(w http.ResponseWriter, r *http.Request) {
-	// vars := mux.Vars(r)
-	// gameId := vars["gameId"]
-	// foundGame, err := game.GetGame(gameId)
-	// check(err)
-	// json.NewEncoder(w).Encode(foundGame)
-=======
 
 	"strconv"
 
@@ -27,5 +18,4 @@ func singleGame(w http.ResponseWriter, r *http.Request) {
 	foundGame, err := game.GetGame(gameIdInt)
 	game.Check(err)
 	json.NewEncoder(w).Encode(foundGame)
->>>>>>> 24f8b899984d104fc2079a3a7f50b5dff41116c8
 }
