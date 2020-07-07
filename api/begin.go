09d6cd07954b5bf6f80455ab912a9e3c9cf8292c
Andrew Horn <arhorn@smcm.edu>
2020-07-07 10:34:11 -0500

    updated api functions

--- api/begin.go
+++ api/begin.go
@@ -3,13 +3,15 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+
+	"../game"
 )
 
 type b struct {
 	ID int `json:"id"`
 }
 
-func begin(w http.ResponseWriter) {
+func begin(w http.ResponseWriter, r *http.Request) {
 	id := game.Begin()
 	json.NewEncoder(w).Encode(b{id})
 }
--- api/makeGuess.go
+++ api/makeGuess.go
@@ -9,14 +9,17 @@ import (
 )
 
 type guess struct {
-	id    string `json:"id"`
-	guess string `json:"guess"`
+	ID    int    `json:"id"`
+	Guess string `json:"guess"`
 }
 
 func makeGuess(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var g guess
 	json.Unmarshal(reqBody, &g)
-	round := game.MakeGuess(g.id, g.guess)
+	round, err := game.MakeGuess(g.ID, g.Guess)
+	if err != nil {
+		json.NewEncoder(w).Encode(err)
+	}
 	json.NewEncoder(w).Encode(round)
 }
