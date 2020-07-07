afb6c1ab1479fc63ca8f53ffcb929ce2998d8cea
Jasmin Smith <jasmith.07.13.99@gmail.com>
2020-07-07 13:38:02 -0400

    new

--- api/listGames.go
+++ api/listGames.go
@@ -1,6 +1,22 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"../game"
+	"github.com/gorilla/mux"
+)
 
 func listGames(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["game"]
+	foundGames := game.ListGames()
+	for _, game := range foundGames {
+		if game.ID == key {
+			json.NewEncoder(w).Encode(account)
+		}
+	}
+	foundGames := game.ListGames()
+	json.NewEncoder(w).Encode(foundGames)
 }
--- api/listRounds.go
+++ api/listRounds.go
@@ -1,7 +1,19 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"../game"
+	"github.com/gorilla/mux"
+)
 
 func listRounds(w http.ResponseWriter, r *http.Request) {
-
+	vars := mux.Vars(r)
+	gameID := vars["gameId"]
+	//convert gameID to int
+	gameIDInt, _ := strconv.Atoi(gameID)
+	foundRounds := game.ListRounds(gameIDInt)
+	json.NewEncoder(w).Encode(foundRounds)
 }
