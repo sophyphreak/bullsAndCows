576a61b71d50685a047f4fa8892c0db2ad5554f5
Andrew Horn <arhorn@smcm.edu>
2020-07-07 09:39:27 -0500

    Added json labeling data to Game and Round

--- game/Round.go
+++ game/Round.go
@@ -4,9 +4,9 @@ import "time"
 
 // Round contains data for one round
 type Round struct {
-	Number         int
-	Guess          int
-	ExactMatches   int
-	PartialMatches int
-	TimeOfGuess    time.Time
+	Number         int       `json:"number"`
+	Guess          int       `json:"guess"`
+	ExactMatches   int       `json:"exactMatches"`
+	PartialMatches int       `json:"partialMatches"`
+	TimeOfGuess    time.Time `json:"timeOfGuess"`
 }
